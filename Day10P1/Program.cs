23d33fbd92ffecbeb8fcedc2876cf9c1d7fc03e6
Kingfrankbob <AJWpcc@gmail.com>
2022-12-10 17:07:59 -0600

    Just work

---
+++ Day10P1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+
+
+class adventCald10p1
+{
+    static void Main(string[] args)
+    {
+        var breakpoints = new int[]
+        {
+          20,
+          60,
+           100,
+           140,
+           180,
+          220
+        };
+        var nextBreakpointId = 0;
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+        string Add = "addx";
+        string Noop = "noop";
+        int cycle = 0;
+        int total = 1;
+        int addVal = 0;
+        int grandTotal = 0;
+        string file = @"C:\Users\ajwpc\Github\ChristmasAdvent\Input.txt";
+        Console.WriteLine(File.Exists(file));
+
+        var lines = File.ReadAllLines(file);
+
+        foreach (var item in lines)
+        {
+
+            if (breakpoints[nextBreakpointId] == cycle ||
+        (item != "noop" && breakpoints[nextBreakpointId] <= cycle + 2))
+            {
+                System.Console.WriteLine($"Signal strength at breakpoint {breakpoints[nextBreakpointId]}: {total * breakpoints[nextBreakpointId]}");
+                System.Console.WriteLine();
+                grandTotal += total * breakpoints[nextBreakpointId];
+                nextBreakpointId++;
+                if (nextBreakpointId >= breakpoints.Length)
+                {
+                    break;
+                }
+
+            }
+
+            if (item.Contains(Noop))
+            {
+                cycle += 1;
+            }
+            if (item.Contains(Add))
+            {
+                var value = item.Split(' ');
+                addVal = int.Parse(value[1]);
+                total += addVal;
+                cycle += 2;
+            }
+        }
+
+        System.Console.Write("Grand Total of all cycles: ");
+        System.Console.WriteLine(grandTotal);
+
+
+    }
+
+}
+
---
+++ Day10P2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+class adventCald10p2
+{
+    static void Main(string[] args)
+    {
+
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+        string Add = "addx";
+        string Noop = "noop";
+        int cycle = 0;
+        int total = 1;
+        int addVal = 0;
+        int placeHold = 0;
+        char[] Screen = new char[240];
+        string file = @"C:\Users\ajwpc\Github\ChristmasAdvent\Day10P1\Input.txt";
+        Console.WriteLine(File.Exists(file));
+
+        var lines = File.ReadAllLines(file);
+
+        foreach (var item in lines)
+        {
+            if (item.Contains(Noop))
+            {
+                cycle += 1;
+            }
+            if (item.Contains(Add))
+            {
+                var value = item.Split(' ');
+                addVal = int.Parse(value[1]);
+                total += addVal;
+                cycle += 2;
+            }
+            try
+            {
+                if (total <= cycle + 1 && cycle - 1 <= total)
+                {
+                    Screen[cycle] = '#';
+                }
+                else
+                    Screen[cycle] = '.';
+
+                System.Console.WriteLine($"Cycle: {cycle} Total: {total} AddVal: {addVal} PlaceHold: {placeHold} Can Place?:{total + 1 == placeHold || total - 1 == placeHold || total == placeHold}");
+            }
+            catch
+
+
+
+
+            placeHold++;
+            }
+            placeHold = 0;
+            foreach (var item in Screen)
+            {
+                Console.Write(item);
+                if (placeHold % 40 == 0)
+                    System.Console.WriteLine();
+                placeHold++;
+            }
+
+
+
+
+        }
+    }
