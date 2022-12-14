713ac489317e1f8c6fda3647cdfdf0090c53aae7
Kingfrankbob <AJWpcc@gmail.com>
2022-12-14 00:52:38 -0600

    Started Day 14

---
+++ Day14/Program.cs
@@ -0,0 +1,117 @@
+﻿// using System.Text;
+namespace Day14
+{
+    class uniqueFinder
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string file = @"Input.txt";
+            Console.WriteLine("File Exists? " + File.Exists(file));
+            var lines = File.ReadAllLines(file);
+            char[,] canvas = new char[100, 200];
+            int counter = 0;
+            int lineCounter = 0;
+
+            var prevx = 0;
+            var prevy = 0;
+
+            int minx = 999, miny = 999, maxx = 0, maxy = 0;
+
+            foreach (var line in lines)
+            {
+                var points = line.Split(" -> ");
+
+
+                foreach (var point in points)
+                {
+                    if (counter != 0)
+                    {
+                        var xy = point.Split(',');
+                        var x = int.Parse(xy[0]) - 460;
+                        var y = int.Parse(xy[1]) - 10;
+
+                        var xdiff = x - prevx;
+                        var ydiff = y - prevy;
+
+                        System.Console.WriteLine("line: " + lineCounter + " x: " + x + " y: " + y + " prevx: " + prevx + " prevy: " + prevy + " xdiff: " + xdiff + " ydiff: " + ydiff);
+
+                        //Graph from prevx,prevy to x,y
+                        if (xdiff == 0)
+                        {
+                            if (ydiff > 0)
+                            {
+                                for (int i = prevy; i <= y; i++)
+                                {
+                                    canvas[prevx, i] = '#';
+                                }
+                            }
+                            else
+                            {
+                                for (int i = y; i <= prevy; i++)
+                                {
+                                    canvas[prevx, i] = '#';
+                                }
+                            }
+                        }
+                        else if (ydiff == 0)
+                        {
+                            if (xdiff > 0)
+                            {
+                                for (int i = prevx; i <= x; i++)
+                                {
+                                    canvas[i, prevy] = '#';
+                                }
+                            }
+                            else
+                            {
+                                for (int i = x; i <= prevx; i++)
+                                {
+                                    canvas[i, prevy] = '#';
+                                }
+                            }
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Error");
+                        }
+
+                    }
+                    var xxy = point.Split(',');
+                    var xx = int.Parse(xxy[0]) - 460;
+                    var yx = int.Parse(xxy[1]) - 10;
+                    prevx = xx;
+                    prevy = yx;
+                    if (counter != 0)
+                    {
+                        if (prevx < minx) minx = prevx;
+                        if (prevx > maxx) maxx = prevx;
+                        if (prevy < miny) miny = prevy;
+                        if (prevy > maxy) maxy = prevy;
+                    }
+
+                    counter++;
+                }
+                counter = 0;
+                lineCounter++;
+
+
+            }
+            counter = 0;
+
+            // System.Console.WriteLine("minx: " + minx + " miny: " + miny + " maxx: " + maxx + " maxy: " + maxy);
+
+
+            foreach (var item in canvas)
+            {
+                if (item != '#') System.Console.Write('.');
+                else System.Console.Write(item);
+
+                if (counter % 100 == 0) System.Console.WriteLine("/n");
+                counter++;
+            }
+
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+namespace Day6
+{
+    class uniqueFinder
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string file = @"Input.txt";
+            Console.WriteLine("File Exists? " + File.Exists(file));
+            var lines = File.ReadAllText(file);
+
+            for (int i = 0; i < lines.Length - 4; i++)
+            {
+                bool isUnique = lines.Skip(i).Take(4).Distinct().Count() == 4;
+                if (isUnique)
+                {
+                    Console.WriteLine("(4)Set of Unique Characters: " + lines.Substring(i, 4) + " at position " + (i + 4));
+                    break;
+                }
+            }
+            for (int i = 0; i < lines.Length - 14; i++)
+            {
+                bool isUnique = lines.Skip(i).Take(14).Distinct().Count() == 14;
+                if (isUnique)
+                {
+                    Console.WriteLine("(14)Set of Unique Characters: " + lines.Substring(i, 14) + " at position " + (i + 14));
+                    break;
+                }
+            }
+
+        }
+    }
+}
