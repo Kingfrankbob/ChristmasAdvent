7639f3f8aeda3319fd85ff0ded0ec64fe7229374
Kingfrankbob <AJWpcc@gmail.com>
2022-12-14 10:29:08 -0600

    kevin

---
+++ Day9/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day5
+{
+    class ropeCalc
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string file = @"Input.txt";
+            Console.WriteLine("File Exists? " + File.Exists(file));
+            var lines = File.ReadAllLines(file);
+
+            
+
+
+
+
+        }
+
+    }
+
+}
