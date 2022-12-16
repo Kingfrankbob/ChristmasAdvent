d556100ee29e865c55400605e09783f9d83ad630
Kingfrankbob <AJWpcc@gmail.com>
2022-12-16 10:56:43 -0600

    More Managment

--- Day12 (Testing)/Program.cs
+++ Day12 (Testing)/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-
-namespace Day12
+﻿namespace Day12
 {
     class hillClimbAlg
     {
