3d3e799d7d0eaa39a1caff5689641771c57b103e
Kingfrankbob <AJWpcc@gmail.com>
2022-12-27 14:07:32 -0600

    ROCK LOGIC

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -14,7 +14,9 @@ namespace Day17
             for (int i = 0; i < 5; i++) Console.WriteLine('\n');
             Console.Clear();
             string file = @"Input.txt";
-            Console.WriteLine("File Exists? " + File.Exists(file) + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("File Exists? " + File.Exists(file) + @"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            ~~~~~~~~~~~~~~~~~
+            ~~~~~~~~~~~~~~~~~");
             var line = File.ReadAllText(file);
             var rockFallen = 0L;
             var counter = 0;
@@ -30,61 +32,93 @@ namespace Day17
                                                   { '#', '#', '#'},
                                                   { '.', '#', '.'} };
 
+            yHitsPerLayer.Add(0, new int[] { 0, 1, 2, 3, 4, 5, 6 });
+
             preRockS.y = 0;
 
-            while (rockFallen < 10)
+            while (rockFallen < 6)
             {
-                Console.WriteLine("Current Rock: " + curRock + " Rocks Fallen: " + rockFallen + " Counter: " + counter + " Height: " + height);
+                Console.WriteLine("Current Rock:" + curRock + " Rocks Fallen:->" + rockFallen + "<---Counter: " + counter + " Height: " + height + " RRRRRRRRRRRRRRRRRRRRRRRRRR");
                 var curRockS = new Rock(curRock);
                 curRock++;
 
                 if (curRock > 5) curRock = 1;
-                if (rockFallen == 0) curRockS.y = botY + 4 + 1;
+                if (rockFallen == 0) curRockS.y = botY + 4;
                 if (rockFallen != 0) curRockS.y = botY + 4;
 
-                for (int i = botY + 4; i > botY; i--)
+                while (!(yHitsPerLayer.ContainsKey(curRockS.y - 1)))
                 {
-                    if (i - botY == 4) System.Console.WriteLine(" Spawn Y: " + curRockS.y);
-                    if (line[jetSteamCounter] == '<')
-                        curRockS.shiftLeft();
-
-                    if (line[jetSteamCounter] == '>')
-                        curRockS.shiftRight();
-                    if (i - botY == 1)
-                    {
-                        if (curRockS.y == botY + 1 && rockFallen > 0)
-                        {
-                            System.Console.Write(" First Collision");
-                            height += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer);
-                            botY += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer);
-                            yHitsPerLayer.Add(botY, curRockS.topXHits);
-                            System.Console.WriteLine(" Layer Height Added: " + (botY) + " Layer Height: " + botY + " Rock Height: " + curRockS.Height + " Hits: " + curRockS.topXHits);
-                            Console.Write("  Stopped? " + height + " JetSteamCounter: " + line[jetSteamCounter] + " I: " + i);
-                        }
-                        else if (rockFallen == 0)
-                        {
-
-                            if (i - 1 == botY)
-                            {
-                                botY += curRockS.Height;
-                                height += curRockS.Height;
-                                preRockS.y += curRockS.Height;
-                                System.Console.Write(" 0 Rock collision");
-                            }
-                        }
-                        else System.Console.Write("       No Collision");
-                    }
-                    curRockS.y--;
-                    Console.Write("             I: " + i + " Current Y: " + curRockS.y + " boty: " + botY + " prevY: " + preRockS.y + " Height: " + height + " Cur x Max: " + curRockS.xMax + " Cur x Min: " + curRockS.xMin + " Current Shape" + curRockS.rockShapeNum + " Line: " + line[jetSteamCounter]);
-                    System.Console.WriteLine();
+                    Console.Write(" Current Y: " + curRockS.y + " boty: " + botY + " Height: " + height + " Cur x Max: " + curRockS.xMax + " Cur x Min: " + curRockS.xMin + " Line: " + line[jetSteamCounter] + " jetCounter: " + jetSteamCounter);
+                    if (line[jetSteamCounter] == '<') { curRockS.shiftLeft(); Console.WriteLine(" Left Shift "); }
+                    if (line[jetSteamCounter] == '>') { curRockS.shiftRight(); Console.WriteLine(" Right Shift "); }
                     jetSteamCounter++;
                     if (jetSteamCounter > line.Length - 1) jetSteamCounter = 0;
-                    counter++;
+                    curRockS.y--;
+                    // Console.WriteLine(" Current Y after shift and drop " + curRockS.y + " boty: " + botY);
+
+                }
+                Console.WriteLine(" Current Y After Falling: " + curRockS.y + " boty: " + botY + " prevY: " + preRockS.y + " Height: " + height + " Cur x Max: " + curRockS.xMax + " Cur x Min: " + curRockS.xMin + " Current Shape" + curRockS.rockShapeNum + " Line: " + line[jetSteamCounter] + " jetCounter: " + jetSteamCounter);
+                jetSteamCounter++;
+                if (curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY) == 2147483647)
+                {
+                    while (curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY) == 2147483647)
+                    {
+                        Console.Write(" Current Y Past: " + curRockS.y + " boty: " + botY + " prevY: " + preRockS.y + " Height: " + height + " Cur x Max: " + curRockS.xMax + " Cur x Min: " + curRockS.xMin + " Current Shape" + curRockS.rockShapeNum + " Line: " + line[jetSteamCounter]);
+                        if (line[jetSteamCounter] == '<') { curRockS.shiftLeft(); Console.WriteLine(" Left Shift "); }
+                        if (line[jetSteamCounter] == '>') { curRockS.shiftRight(); Console.WriteLine(" Right Shift "); }
+                        jetSteamCounter++;
+                        if (jetSteamCounter > line.Length - 1) jetSteamCounter = 0;
+                        curRockS.y--;
+                    }
+                    height += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                    botY += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                    yHitsPerLayer = Rock.addRockHits(curRockS, yHitsPerLayer, botY);
+                }
+                else
+                {
+                    height += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                    botY += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                    yHitsPerLayer = Rock.addRockHits(curRockS, yHitsPerLayer, botY);
                 }
 
-                preRockS = curRockS;
 
+
+                preRockS = curRockS;
                 rockFallen++;
+
+                // for (int i = botY + 4; i > botY; i--)
+                // {
+                //     if (i - botY == 4) System.Console.WriteLine(" Spawn Y: " + curRockS.y);
+
+                //     if (i - botY == 1)
+                //     {
+                //         if (curRockS.y == botY + 1 && rockFallen > 0)
+                //         {
+                //             height += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                //             botY += curRockS.hasCollided(curRockS, preRockS, yHitsPerLayer, botY);
+                //             yHitsPerLayer = Rock.addRockHits(curRockS, yHitsPerLayer, botY);
+                //             // Console.Write("  Stopped? " + height + " JetSteamCounter: " + line[jetSteamCounter] + " I: " + i);
+                //         }
+                //         else if (rockFallen == 0)
+                //         {
+
+                //             if (i - 1 == botY)
+                //             {
+                //                 botY += curRockS.Height;
+                //                 height += curRockS.Height;
+                //                 preRockS.y += curRockS.Height;
+                //                 // System.Console.Write(" 0 Rock collision");
+                //             }
+                //         }
+                //         else System.Console.Write("       No Collision");
+                //     }
+                //     curRockS.y--;
+                //     System.Console.WriteLine();
+                //     jetSteamCounter++;
+                //     if (jetSteamCounter > line.Length - 1) jetSteamCounter = 0;
+                //     counter++;
+                // }
+
 
             }
             Console.WriteLine(" Rocks Fallen: " + rockFallen + " Height: " + height);
@@ -97,7 +131,7 @@ namespace Day17
             public int xMin;
             public int xMax;
             public int[] topXHits = new int[] { };
-            public int[] midXHits = new int[] { };
+            public int[]? midXHits = new int[] { };
             public int[] botXHits = new int[] { };
             public int y;
             // public int prevY;
@@ -135,6 +169,7 @@ namespace Day17
                     case 1:
                         shape = rockOne;
                         topXHits = new int[] { 0, 1, 2, 3 };
+                        midXHits = null;
                         botXHits = new int[] { 0, 1, 2, 3 };
                         xMax = 5;
                         xMin = 2;
@@ -158,6 +193,7 @@ namespace Day17
                     case 4:
                         shape = rockFour;
                         topXHits = new int[] { 0 };
+                        midXHits = null;
                         botXHits = new int[] { 0 };
                         xMax = 2;
                         xMin = 2;
@@ -166,6 +202,7 @@ namespace Day17
                         shape = rockFive;
                         topXHits = new int[] { 0, 1 };
                         botXHits = new int[] { 0, 1 };
+                        midXHits = null;
                         xMax = 3;
                         xMin = 2;
                         break;
@@ -196,12 +233,13 @@ namespace Day17
 
                     }
                     counter = 0;
-                    foreach (var val in midXHits)
-                    {
-                        midXHits[counter] = val - 1;
-                        counter++;
+                    if (!(midXHits is null))
+                        foreach (var val in midXHits)
+                        {
+                            midXHits[counter] = val - 1;
+                            counter++;
 
-                    }
+                        }
 
                 }
             }
@@ -224,11 +262,12 @@ namespace Day17
                         counter++;
                     }
                     counter = 0;
-                    foreach (var val in midXHits)
-                    {
-                        midXHits[counter] = val + 1;
-                        counter++;
-                    }
+                    if (!(midXHits is null))
+                        foreach (var val in midXHits)
+                        {
+                            midXHits[counter] = val + 1;
+                            counter++;
+                        }
                 }
             }
             public int Width
@@ -252,114 +291,70 @@ namespace Day17
                         return 0;
                 }
             }
-            public int hasCollided(Rock currentRock, Rock previousRock, Dictionary<int, int[]> yHitsPerLayer)
+            public int hasCollided(Rock currentRock, Rock previousRock, Dictionary<int, int[]> yHitsPerLayer, int botY)
             {
                 if (currentRock == null || previousRock == null)
                 {
                     Console.WriteLine("Error: Rock is null! Current: " + currentRock + " Previous: " + previousRock);
                     return 0;
                 }
-                var sameLayerHit = true;
 
-                if (currentRock.rockShapeNum != 2 && previousRock.rockShapeNum != 2)
+                var hitsCurrent = yHitsPerLayer[currentRock.y - 1];
+                // Console.WriteLine("Y: " + currentRock.y);
+
+                foreach (var item in currentRock.botXHits)
                 {
-                    foreach (var item in currentRock.botXHits)
+                    if (hitsCurrent.Contains(item))
                     {
-                        foreach (var top in previousRock.topXHits)
+                        // System.Console.WriteLine("Collision on bottom end {0} RockBottom at: {1} and boty: {2} with the height being {3}", currentRock.y, currentRock.y, botY, currentRock.Height);
+                        // Console.Write("Collision on bottom end {0}", currentRock.y);
+                        if (currentRock.y < botY)
                         {
-                            if (item == top)
-                            {
-                                Console.WriteLine("Bot hit r2 {0}", item);
-                                return currentRock.Height;
-                            }
+                            return currentRock.y + currentRock.Height - 1 - botY;
                         }
-                    }
-                    sameLayerHit = false;
+                        return currentRock.Height;
 
+                    }
                 }
 
-                else if (currentRock.rockShapeNum == 2)
-                {
-                    foreach (var item in currentRock.botXHits)
-                    {
-                        if (previousRock.topXHits.Contains(item))
-                        {
-                            Console.WriteLine("Bot hit c2 {0}", item);
-                            return currentRock.Height;
-                        }
-                    }
+                // Console.WriteLine("No hit");
+                return 2147483647;
 
-                    foreach (var item in currentRock.midXHits)
+
+
+            }
+
+            public static Dictionary<int, int[]> addRockHits(Rock currentRock, Dictionary<int, int[]> yHitsPerLayer, int botY)
+            {
+                if (currentRock == null)
+                {
+                    Console.WriteLine("Error: Rock is null! Current: " + currentRock);
+                    return yHitsPerLayer;
+                }
+                try
+                {
+                    if (currentRock.midXHits == null)
                     {
-                        if (previousRock.topXHits.Contains(item))
-                        {
-                            Console.WriteLine("Mid hit c2 {0}", item);
-                            return currentRock.Height - 1;
-                        }
+                        yHitsPerLayer.Add(botY, currentRock.topXHits);
+                        yHitsPerLayer.Add(botY - 1, currentRock.botXHits);
                     }
-                    sameLayerHit = false;
+                    else
+                    {
+                        yHitsPerLayer.Add(botY, currentRock.topXHits);
+                        yHitsPerLayer.Add(botY - 1, currentRock.midXHits);
+                        yHitsPerLayer.Add(botY - 2, currentRock.botXHits);
+                    }
+                }
+                catch (Exception)
+                {
+                    var prev = yHitsPerLayer[botY];
+                    yHitsPerLayer.Remove(botY);
+                    List<int> newHits = new List<int>(currentRock.topXHits);
+                    newHits.AddRange(prev);
+                    yHitsPerLayer.Add(botY, newHits.ToArray());
                 }
 
-                else if (previousRock.rockShapeNum == 2)
-                {
-                    foreach (var item in previousRock.topXHits)
-                    {
-                        if (currentRock.botXHits.Contains(item))
-                        {
-                            Console.WriteLine("Bot hit p2 {0}", item);
-                            return currentRock.Height;
-                        }
-                    }
-
-                    foreach (var item in previousRock.midXHits)
-                    {
-                        if (currentRock.botXHits.Contains(item))
-                        {
-                            Console.WriteLine("Mid hit p2  {0}", item);
-                            return currentRock.Height - 1;
-                        }
-                    }
-                    sameLayerHit = false;
-                }
-                else
-                {
-                    Console.WriteLine("Error: Rock shape not found! (Collision)");
-                    return 0;
-                }
-
-                while (!sameLayerHit)
-                {
-                    var hitsCurrent = yHitsPerLayer[currentRock.y - 1];
-                    foreach (var item in currentRock.botXHits)
-                    {
-                        if (hitsCurrent.Contains(item))
-                        {
-                            Console.WriteLine("Bot hit Special {0}", item);
-                            return currentRock.Height;
-                        }
-                    }
-
-                    foreach (var item in hitsCurrent)
-                    {
-                        System.Console.WriteLine(item);
-
-                    }
-                    System.Console.WriteLine(" Hits Current" + hitsCurrent.Length);
-                    foreach (var item in currentRock.botXHits)
-                    {
-                        System.Console.WriteLine(item);
-                    }
-                    System.Console.WriteLine(" Bot X Hits" + currentRock.botXHits.Length);
-
-                    while (true) { }
-
-                }
-
-                Console.WriteLine("No hit");
-                return 0;
-
-
-
+                return yHitsPerLayer;
             }
 
 
